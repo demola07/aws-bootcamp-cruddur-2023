68865b91df79a8cbb41a498f9586159fec17828d
Ademola <demolasobaki@gmail.com>
2023-03-11 13:15:30 +0100

    verify jwt token in backend flask app

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,8 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 
 # HoneyComb *************************
 from opentelemetry import trace
@@ -84,14 +86,20 @@ RequestsInstrumentor().instrument()
 # X-Ray *****************************
 # XRayMiddleware(app, xray_recorder)
 
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": "*"}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -144,7 +152,19 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 # @xray_recorder.capture('activities_home')
 def data_home():
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
--- journal/week3.md
+++ journal/week3.md
@@ -1,1 +1,40 @@
 # Week 3 — Decentralized Authentication
+
+## Provision Amazon Cognito User Pool
+
+I was able to provision a coginto user pool 
+
+ ![cognito_user_pool](./assets//cognito_user_pool.JPG)
+
+ I then was able to  create a user, after implementing the signin, signup and recovery pages on the frontend
+
+ ![cognito_user_signin](./assets//congito_signin.JPG)
+
+ I got the verification mail in my mailbox
+
+ ![cognito_verification_mail](./assets//cognito_verification_mailJPG.JPG)
+
+ ![cognito_verification_mail](./assets//congito_verification.JPG)
+
+And I was able to successfully signin
+
+ ![cognito_verification_mail](./assets//cognito_signin.JPG)
+
+Confirmation of user on aws cognito
+
+ ![cognito_user](./assets//cognito_user_confirmation.JPG)
+
+ ### Cognito JWT Server Side verification
+
+ I was able to folow the instructions and successfully implement the server side jwt verification.
+
+  *Signed in*
+ ![cognito_jwt_verify](./assets//cognito_jwt_verify.JPG)
+
+  *Signed out*
+ ![cognito_jwt_verify](./assets//cognito_jwt_verify1.JPG)
+
+
+
+
+
