a33c71284357c1025f579cd35895f6fd6b4631b2
Ademola <demolasobaki@gmail.com>
2023-03-16 20:58:53 +0100

    implement first postgres query

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,18 @@
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Andrew Bayko', 'bayko' ,'MOCK'),
+  ('Ademola Sobaki', 'demola07' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'demola07' LIMIT 1),
+    'This was imported as seed data new!',
     current_timestamp + interval '10 day'
   )
