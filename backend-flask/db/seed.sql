7165385b93ee320a2e31b3be7694662b3805254c
Ademola <demolasobaki@gmail.com>
2023-03-23 11:15:08 +0100

    implement bash commands to load schema and seed data to dynamodb local instance

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email,  handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK'),
-  ('Ademola Sobaki', 'demola07' ,'MOCK');
+  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko', 'bayko@exampro.co','bayko' ,'MOCK'),
+  ('Ademola Sobaki', 'ademola@exampro.co', 'demola07' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
